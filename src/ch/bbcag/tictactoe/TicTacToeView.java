a59874fd7aad393da0be050f5a8c590ec01a1e49
zimrea <zimrea@WS-ZIMREA.bbcnet-zh.local>
2018-01-29 14:12:14 +0100

    view added

---
+++ src/ch/bbcag/tictactoe/TicTacToeView.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package ch.bbcag.tictactoe;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zimrea
+ *
+ */
+public class TicTacToeView extends JFrame {
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public TicTacToeView() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public TicTacToeView(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @throws HeadlessException
+	 */
+	public TicTacToeView(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @param gc
+	 */
+	public TicTacToeView(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
