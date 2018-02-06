33b449d30d54c212555cc8eb840d14def9d48a93
zmaurn <zmaurn@WS-ZMAURN.bbcnet-zh.local>
2018-02-06 09:35:12 +0100

    ka

---
+++ src/ch/bbcag/tictactoe/Game.java
@@ -0,0 +1,15 @@
+package ch.bbcag.tictactoe;
+
+public class Game {{
+	int gamemode = 2;
+	
+	if(gamemode==0)
+	{
+		//Gui anzeigen
+		// Computer-Logik  s
+	}else
+	{	
+		//Gui anzeigen
+		// Spieler-Logik
+	}
+}}
