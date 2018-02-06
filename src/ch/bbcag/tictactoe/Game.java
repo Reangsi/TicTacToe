734f134ec67404518c1235f3430ae75a8c2e42f6
zmaurn <zmaurn@WS-ZMAURN.bbcnet-zh.local>
2018-02-06 11:16:27 +0100

    Game Fortschritt

--- src/ch/bbcag/tictactoe/Game.java
+++ src/ch/bbcag/tictactoe/Game.java
@@ -1,7 +1,12 @@
 package ch.bbcag.tictactoe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {{
 	int gamemode = 2;
+	boolean checkForWinningRow;
+	List<String> board = new ArrayList<>();
 	
 	if(gamemode==0)
 	{
@@ -10,6 +15,17 @@ public class Game {{
 	}else
 	{	
 		//Gui anzeigen
-		// Spieler-Logik
+		if (board == [20][21][22] or board == [10][11][12] or board == [00][01][02] or board == [00][11][22] or board == [20][11][02] or board == [00][10][20] or board == [01][11][21] or board == [02][12][22]) {
+			//gameover
+			if (player == "1") {
+				System.out.println("Player 1 hat gewonnen!");
+			}
+			else {
+				System.out.println("Player 2 hat gewonnen!");
+			}
+			
+		}
+		
 	}
-}}
+}
+}
