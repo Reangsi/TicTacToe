e4803ae1ef71992a7e444327cae459bc89ed7890
zmaurn <zmaurn@WS-ZMAURN.bbcnet-zh.local>
2018-02-13 09:26:56 +0100

    Variante 2 hinzugefügt

--- src/ch/bbcag/tictactoe/ComputerVsPlayer.java
+++ src/ch/bbcag/tictactoe/ComputerVsPlayer.java
@@ -72,8 +72,64 @@ public class ComputerVsPlayer {
 	// }
 	// }
 	//
-	// else if (PlayerTurn == bf2) { // Diese Funktion wird noch gemacht
+	// else if (PlayerTurn == 2) { // Diese Funktion wird noch gemacht
 	// spielfeld[2][0] = computer;
 	// }
 	// }
+
+	public void playerSetztEcke() {
+		if (spielfeld[0][0] == player || spielfeld[0][2] == player || spielfeld[2][0] == player
+				|| spielfeld[2][2] == player) {
+			if (PlayerTurn == 1) {
+				spielfeld[1][1] = computer;
+			} else if (PlayerTurn == 2) {
+				if ((spielfeld[0][0] == player && spielfeld[2][1] == player)
+						|| (spielfeld[2][1] == player && spielfeld[0][2] == player)
+						|| (spielfeld[0][0] == player && spielfeld[1][2] == player)
+						|| (spielfeld[1][0] == player && spielfeld[0][2] == player)
+						|| (spielfeld[2][0] == player && spielfeld[0][1] == player)
+						|| (spielfeld[0][1] == player && spielfeld[2][2] == player)
+						|| (spielfeld[2][0] == player && spielfeld[1][2] == player)
+						|| (spielfeld[1][0] == player && spielfeld[2][2] == player)) {
+
+					if (spielfeld[2][0] == player) {
+						spielfeld[2][2] = computer;
+					}
+
+					else {
+						spielfeld[2][0] = computer;
+					}
+				}
+
+				else {
+					spielfeld[2][1] = computer; // Dieses Feld sollte frei sein, sonst sollte es gar nicht zu dieser
+					// Selektion kommen.
+				}
+			}
+		}
+	}
+
+	public void playerSetztNormal() {
+		if (spielfeld[0][1] == player || spielfeld[1][2] == player || spielfeld[1][0] == player
+				|| spielfeld[2][1] == player) {
+			if (PlayerTurn == 1) {
+
+				if (spielfeld[0][1] == player) {
+					spielfeld[2][1] = computer;
+				}
+
+				if (spielfeld[1][0] == player) {
+					spielfeld[1][2] = computer;
+				}
+
+				if (spielfeld[2][1] == player) {
+					spielfeld[0][1] = computer;
+				}
+
+				if (spielfeld[1][2] == player) {
+					spielfeld[1][0] = computer;
+				}
+			}
+		}
+	}
 }
