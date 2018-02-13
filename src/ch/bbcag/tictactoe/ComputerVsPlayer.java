d34bbb8ff8b5695f9518efb595679506fc2e8baf
zmaurn <zmaurn@WS-ZMAURN.bbcnet-zh.local>
2018-02-13 12:07:09 +0100

    playerTurn namens änderung

--- src/ch/bbcag/tictactoe/ComputerVsPlayer.java
+++ src/ch/bbcag/tictactoe/ComputerVsPlayer.java
@@ -4,6 +4,7 @@ public class ComputerVsPlayer {
 	private String player;
 	private String computer;
 	private String[][] spielfeld = new String[3][3];
+	private int playerTurn;
 
 	public void checkForTwoInARow() {
 		if ((spielfeld[0][1] == player && spielfeld[0][2] == player)
@@ -80,9 +81,9 @@ public class ComputerVsPlayer {
 	public void playerSetztEcke() {
 		if (spielfeld[0][0] == player || spielfeld[0][2] == player || spielfeld[2][0] == player
 				|| spielfeld[2][2] == player) {
-			if (PlayerTurn == 1) {
+			if (playerTurn == 1) {
 				spielfeld[1][1] = computer;
-			} else if (PlayerTurn == 2) {
+			} else if (playerTurn == 2) {
 				if ((spielfeld[0][0] == player && spielfeld[2][1] == player)
 						|| (spielfeld[2][1] == player && spielfeld[0][2] == player)
 						|| (spielfeld[0][0] == player && spielfeld[1][2] == player)
@@ -112,7 +113,7 @@ public class ComputerVsPlayer {
 	public void playerSetztNormal() {
 		if (spielfeld[0][1] == player || spielfeld[1][2] == player || spielfeld[1][0] == player
 				|| spielfeld[2][1] == player) {
-			if (PlayerTurn == 1) {
+			if (playerTurn == 1) {
 
 				if (spielfeld[0][1] == player) {
 					spielfeld[2][1] = computer;
